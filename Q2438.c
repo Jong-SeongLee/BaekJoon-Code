4528f3d9aa586ba86592654911b4933d20478df0
Jong-SeongLee <leejs5485@hanmail.net>
2021-11-25 01:18:01 +0900

    Q2438 Commit

---
+++ Q2438.c
@@ -0,0 +1,15 @@
+//백준 2438번 풀이 
+#include <stdio.h>
+int main(){
+	int a=0;
+	scanf("%d",&a);
+	int i, j;
+	for(i=0;i<a;i++){
+		for(j=0;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
